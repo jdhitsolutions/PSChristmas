9da40214ea6a970e65b798b23a3b343e3f24aa38
jdhitsolutions <jhicks@jdhitsolutions.com>
2016-12-20 08:36:02 -0500

    v0.0.1 Initial build

---
+++ PSChristmas.psm1
@@ -0,0 +1,94 @@
+﻿#requires -version 5.0
+
+<#
+A demo PowerShell class based module
+#>
+
+#dot source helper functions
+. .\PSChristmasFunctions.ps1
+
+Enum ListStatus {
+    Naughty
+    Nice
+}
+
+Enum Present {
+    Socks
+    Barbie
+    Elmo
+    XBox
+    GIJoe
+    Underwear
+    HotWheels
+    EZBakeOven
+    Walkman
+    PS4
+    Pajamas
+    Stratego
+    GinzuKnives
+    ChiaPet
+    Tie
+    Mittens
+    Bicycle
+    Pony
+    Battleship
+    CabbagePatchDoll
+}
+
+Class myChristmas {
+
+#class properties
+[string]$Greeting
+[string]$ElfName
+[ListStatus]$List
+[int]$DaysRemaining
+[string]$CountDown
+
+#class methods
+[string]GetGreeting() {
+  $text = New-Greeting
+  return $text
+}
+
+
+
+[void]Refresh() {
+ #calculate christmas for the current year that should be culture aware
+ $Christmas = [datetime]::new( (Get-Date).Year,12,25)
+ $span = $Christmas - (Get-Date)
+ $this.DaysRemaining = $span.totalDays
+ #strip off milliseconds
+ $timestring = $span.ToString()
+ $this.CountDown = $timestring.Substring(0,$timestring.LastIndexOf("."))
+ 
+ $this.Greeting = New-Greeting
+}
+
+
+[void]Play() {
+    PlayTune
+}
+
+[void]Show() {
+    ShowGraphic
+}
+
+#class constructor
+myChristmas() {
+ $this.ElfName = New-ElfName
+
+ if ( (Get-Date).Second%2) {
+    $this.List = [ListStatus]::Naughty
+ }
+ else {
+    $this.List = [ListStatus]::Nice
+ }
+ 
+ #set the rest of the properties by invoking the defined
+ #Refresh() method
+
+ $this.Refresh()
+}
+
+}
+
