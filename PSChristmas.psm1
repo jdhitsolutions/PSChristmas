c54a00b87fadc86d2bed47dd77b4ec9c18e925e7
jdhitsolutions <jhicks@jdhitsolutions.com>
2016-12-20 09:04:06 -0500

    v0.0.3 added functions to create Christmas object moved text lists to a json file Updated manifest

--- PSChristmas.psm1
+++ PSChristmas.psm1
@@ -45,49 +45,49 @@ Class myChristmas {
 [string]$CountDown
 
 #class methods
-[string]GetGreeting() {
-  $text = New-Greeting
-  return $text
-}
-
-
-
 [void]Refresh() {
+ Write-Verbose "[CLASS] Invoking Refresh()"
  #calculate christmas for the current year that should be culture aware
  $Christmas = [datetime]::new( (Get-Date).Year,12,25)
+
+ Write-Verbose "[CLASS] Christmas this year is $Christmas"
  $span = $Christmas - (Get-Date)
  $this.DaysRemaining = $span.totalDays
  #strip off milliseconds
  $timestring = $span.ToString()
  $this.CountDown = $timestring.Substring(0,$timestring.LastIndexOf("."))
- 
+
+ Write-Verbose "[CLASS] Getting new greeting"
  $this.Greeting = New-Greeting
 }
 
 
 [void]Play() {
+    Write-Verbose "[CLASS] Invoking Play()"
     PlayTune
 }
 
 [void]Show() {
+    Write-Verbose "[CLASS] Invoking Show()"
     ShowGraphic
 }
 
 #class constructor
 myChristmas() {
- $this.ElfName = New-ElfName
+    Write-Verbose "[CLASS] Invoking Constructor"
+     $this.ElfName = New-ElfName
 
- if ( (Get-Date).Second%2) {
-    $this.List = [ListStatus]::Naughty
- }
- else {
-    $this.List = [ListStatus]::Nice
- }
+     if ( (Get-Date).Second%2) {
+        $this.List = [ListStatus]::Naughty
+     }
+     else {
+        $this.List = [ListStatus]::Nice
+     }
  
- #set the rest of the properties by invoking the defined
- #Refresh() method
+     #set the rest of the properties by invoking the defined
+     #Refresh() method
 
- $this.Refresh()
+     $this.Refresh()
 }
 
 }
